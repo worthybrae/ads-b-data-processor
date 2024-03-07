bfd43d886dd5689688f1ebd9d8a291de50c84bb5
worthybrae <worthyrae@gmail.com>
2024-03-07 13:02:58 -0500

    initial structure

--- adsb.py
+++ adsb.py
@@ -2,62 +2,107 @@ import socket
 import sys
 import subprocess
 import time
-import json
 import os
 from datetime import datetime
 import hashlib
 import pandas as pd
-import threading
+
+
+def sbs_parser(string):
+    cut_words = [
+        'MSG',
+        'CLK',
+        'STA',
+        'AIR',
+        'ID',
+        'SEL'
+    ]
+    cleaned_string = string.decode('utf-8').strip().replace(' ','').replace('\r', '').replace('\n', '')
+    string_parts = cleaned_string.split(',')
+    message_list = []
+    if len(string_parts) > 0:
+        current_message = []
+        for part in string_parts:
+            formatted_part = part.strip()
+            if not current_message:
+                current_message.append(formatted_part)
+            elif formatted_part in cut_words:
+                message_list.append(current_message)
+                current_message = [formatted_part]
+            else:
+                current_message.append(formatted_part)
+    return message_list
+
 
 
 class ADSBData:
-    def __init__(self, adsb_message_bytes):
-        # Decode byte string to string
-        message_str = adsb_message_bytes.decode('utf-8').strip()
-        # Split the message by commas to parse the fields
-        message_parts = message_str.split(',')
-
-        # Extract relevant data
-        self.message_type = message_parts[0]
-        self.transmission_type = message_parts[1]
-        self.session_id = message_parts[2]
-        self.aircraft_id = message_parts[3]
-        self.hex_ident = message_parts[4]
-        self.flight_id = message_parts[5]
-        self.date_message_generated = message_parts[6]
-        self.time_message_generated = message_parts[7]
-        self.date_message_logged = message_parts[8]
-        self.time_message_logged = message_parts[9]
-        # Depending on the transmission type, other fields may be populated
-        self.altitude = int(message_parts[11]) if message_parts[11] else None
-        self.latitude = float(message_parts[14]) if message_parts[14] else None
-        self.longitude = float(message_parts[15]) if message_parts[15] else None
-        self.callsign = message_parts[10].strip() if message_parts[10] else None
-        # Generate a unique ID for the data point (customize as needed)
-        self.unique_id = hashlib.md5(f"{self.hex_ident}{self.date_message_logged}{self.time_message_logged}".encode()).hexdigest()
+    def __init__(self, message):
+        try:
+            # Extract relevant data
+            self.message_type = message[0]
+            self.aircraft_type = message[3]
+            self.aircraft_id = message[4]
+            self.flight_id = message[5]
+            self.timestamp = datetime.strptime(f"{message[8]} {message[9]}", "%Y/%m/%d %H:%M:%S.%f") 
+            # Depending on the transmission type, other fields may be populated
+            self.altitude = int(message[11]) if message[11] else None
+            self.ground_speed = int(message[12]) if message[12] else None
+            self.track = int(message[13]) if message[13] else None
+            self.latitude = float(message[14]) if message[14] else None
+            self.longitude = float(message[15]) if message[15] else None
+            self.callsign = message[10].strip() if message[10] else None
+            # Generate a unique ID for the data point (customize as needed)
+            ifl_hash_str = f"{self.aircraft_id}{self.latitude:.4f}{self.longitude:.4f}{self.timestamp.strftime('%Y-%m-%d %H:%M')}"
+            self.ifl_hash = hashlib.md5(ifl_hash_str.encode()).hexdigest()
+            dup_hash_str = f"{self.aircraft_id}{self.latitude}{self.longitude}{self.timestamp.strftime('%Y-%m-%d %H:%M:%S.%f')}"
+            self.dup_hash = hashlib.md5(dup_hash_str.encode()).hexdigest()
+            self.ingested_at = datetime.utcnow()
+        except:
+            self.aircraft_id = None
+            self.message_type = None
+            self.latitude = None
+            self.longitude = None
+            self.timestamp = None
 
     def __str__(self):
-        return f"Hex Ident: {self.hex_ident}, Callsign: {self.callsign}, Altitude: {self.altitude}, Latitude: {self.latitude}, Longitude: {self.longitude}"
+        return f"Aircraft ID: {self.aircraft_id}, Latitude: {self.latitude}, Longitude: {self.longitude}, Timestamp: {self.timestamp}"
+
+    def validate(self):
+        if self.message_type == 'MSG' and self.aircraft_id and self.latitude and self.longitude and self.timestamp:
+            return True
+        else:
+            return False
 
 class ADSBDataStream:
     def __init__(self):
         self.messages = []
         self.last_hour = None
+        self.last_date = None
 
     def add_new_message(self, message):
         self.messages.append([
-            message.flight_number,
+            message.aircraft_id,
+            message.aircraft_type,
+            message.flight_id,
+            message.timestamp,
             message.altitude,
-            message.speed,
+            message.ground_speed,
+            message.track,
             message.latitude,
             message.longitude,
-            message.timestamp,
-            message.unique_id
+            message.callsign,
+            message.ifl_hash,
+            message.dup_hash,
+            message.ingested_at
         ])
         self.check_if_new_hour(message.timestamp.hour, message.timestamp.strftime('%Y-%m-%d'))
 
     def check_if_new_hour(self, hour, date):
-        if self.last_hour is None or int(hour) != self.last_hour:
+        if self.last_hour is None:
+            self.last_hour = int(hour)
+            self.last_date = date
+        
+        elif int(hour) != self.last_hour:
             self.save_messages()
             self.last_hour = int(hour)
             self.last_date = date
@@ -67,13 +112,19 @@ class ADSBDataStream:
             return
 
         df = pd.DataFrame(self.messages, columns=[
-            'flight_number', 
-            'altitude', 
-            'speed', 
-            'latitude', 
-            'longitude', 
+            'aircraft_id',
+            'aircraft_type',
+            'flight_id',
             'timestamp',
-            'unique_id'
+            'altitude',
+            'ground_speed',
+            'track',
+            'latitude',
+            'longitude',
+            'callsign',
+            'ifl_hash',
+            'dup_hash',
+            'ingested_at'
         ])
 
         year, month, day = self.last_date.split('-')
@@ -114,47 +165,44 @@ def run_dump1090():
         print(f"Failed to start dump1090-mutability: {e}")
         sys.exit(1)
 
-def receive_aggregated_data(connection):
+
+def receiver():
+    # run_dump1090() # use to make sure background process is feeding data to the socket
     while True:
-        data = connection.recv(1024)  # Receive aggregated data
-        row = ADSBData(data)
-        print(f"Received aggregated data: {row}")
-    connection.close()
-    print("Disconnected from aggregated data feed.")
+        try:
+            # Attempt to establish a connection
+            aggregated_data = SocketConnection('out.adsb.lol', 1338, 'aggregated data feed')
+            aggregated_data_stat, aggregated_data_conn = aggregated_data.create_connection()
 
-def run():
-    run_dump1090()
+            if aggregated_data_stat:
+                print("Connection established to aggregated data feed.")
+                stream = ADSBDataStream()
 
-    # Connect to ADS-B broadcaster
-    adsbhub = SocketConnection('data.adsbhub.org', 5001, 'ads-b broadcaster')
-    adsbhub_stat, adsbhub_conn = adsbhub.create_connection()
+                while True:
+                    data = aggregated_data_conn.recv(1024)
+                    if not data:
+                        raise ConnectionError("No data received. Connection might be lost.")
+                    
+                    message_list = sbs_parser(data)
+                    if message_list:
+                        for message in message_list:
+                            adsb_obj = ADSBData(message)
+                            if adsb_obj.validate():
+                                print(adsb_obj)
+                                stream.add_new_message(adsb_obj)
 
-    # Connect to ADS-B receiver
-    dump1090 = SocketConnection('localhost', 30003, 'ads-b receiver')
-    dump1090_stat, dump1090_conn = dump1090.create_connection()
+            else:
+                # If connection was not successful, print an error and try to reconnect
+                print("Failed to connect to the aggregated data feed. Retrying...")
+                time.sleep(5)
+                continue
 
-    # Connect to aggregated data feed
-    aggregated_data = SocketConnection('data.adsbhub.org', 5002, 'aggregated data feed')
-    aggregated_data_stat, aggregated_data_conn = aggregated_data.create_connection()
-
-    # Handle aggregated data in a separate thread
-    if aggregated_data_stat:
-        threading.Thread(target=receive_aggregated_data, args=(aggregated_data_conn,), daemon=True).start()
-
-    # Handle local ADS-B data and forward to ADS-B hub
-    if adsbhub_stat and dump1090_stat:
-        while True:
-            data = dump1090_conn.recv(1024)  # Receive data from dump1090
-            if not data:
-                break
-            adsbhub_conn.sendall(data)  # Forward data to ADS-B Hub
-    else:
-        print(f"Error connecting to services...")
-
-    # Cleanup
-    dump1090_conn.close()
-    adsbhub_conn.close()
-    print("Disconnected from ADS-B broadcaster and receiver.")
+        except (ConnectionError, socket.error) as e:
+            print(f"Connection error: {e}. Reconnecting in 5 seconds...")
+            time.sleep(5)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            break  # Exit the loop if a non-recoverable error occurs
 
 if __name__ == '__main__':
-    run()
+    receiver()
